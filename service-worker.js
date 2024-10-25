486a443b0ba48d638b18b466cb5819ab1fe199bb
Jasper <jaspergatila2@gmail.com>
2024-10-25 21:35:25 +0800

    third commit

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,16 @@ self.addEventListener("install", e =>{
   );
 });
 
-self.addEventListener("fetch", e =>{
-  e.respondWith(caches.match(e.request).then(response =>{
-    return response || fetch(e.request);
-  }));
+// self.addEventListener("fetch", e =>{
+//   e.respondWith(caches.match(e.request).then(response =>{
+//     return response || fetch(e.request);
+//   }));
+// });
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request).catch(() => caches.match("./fallback.html"));
+    })
+  );
 });
